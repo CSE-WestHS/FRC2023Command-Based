d565e37f4a2356e5380ee86923835d0a4ad0efeb
superheropug <71048116+superheropug@users.noreply.github.com>
2023-01-15 16:39:40 -0600

    Lots of code
    PhotonVision and NavChip functionality

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.NavchipManager;
 
 
 /**
@@ -17,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  NavchipManager navX;
   private RobotContainer m_robotContainer;
 
   /**
@@ -29,6 +30,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    navX = new NavchipManager();
+    
     
   }
 
@@ -46,6 +49,9 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    navX.update();
+    navX.displayAllAxes();
+
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
