226216a385bfdf487e3af9d07bc519c7fb896733
Atika2022 <114194972+Atika2022@users.noreply.github.com>
2023-02-02 17:20:54 -0600

    ChoosingAutonomous

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.NavchipManager;
+import frc.robot.subsystems.NavchipManager; 
+
 
 
 /**
@@ -100,4 +101,6 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.commands.BasicDrive.DriveBackward;
@@ -16,7 +18,7 @@ import frc.robot.commands.Dual_Joysticks;
 import frc.robot.commands.BasicAuto;
 
 import frc.robot.subsystems.DriveSubsystem;
-
+  
 
 
 /**
@@ -37,6 +39,12 @@ public class RobotContainer {
   
   private final BasicAuto Auto = new BasicAuto(DriveSubsystem);
 
+  private final Command m_BasicAuto1 = DriveFiveRotations;
+  private final Command m_BasicAuto2 = DriveThreeRotoations;
+  private final Command m_BasicAuto3 = Turn90Degrees;
+  private final Command m_BasicAuto4 = Turn180Degrees;
+
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -44,9 +52,21 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
     DriveSubsystem.setDefaultCommand(Dual_Joysticks);
+    SmartDashboard.putData(DriveSubsystem);
+    SmartDashboard.putData("BasicAuto", new BasicAuto(DriveSubsystem));
+    SmartDashboard.putData("DriveBackward", new DriveBackward(DriveSubsystem, 5, 0.5));
+    SmartDashboard.putData("DriveForward", new DriveForward(DriveSubsystem, 5, 0.5));
+    SmartDashboard.putData("TurnLeft", new TurnLeft(DriveSubsystem, 5, 0.5));
+    SmartDashboard.putData("TurnRight", new TurnRight(DriveSubsystem, 5, 0.5));
+    
+  m_chooser.setDefaultOption("DriveBackward", m_BasicAuto1);
+  m_chooser.addOption("DriveForward", m_BasicAuto2);
+  m_chooser.addOption("TurnLeft", m_BasicAuto3);
+  m_chooser.addOption("TurnRight", m_BasicAuto4);
+
+  SmartDashboard.putData(m_chooser);
 
   }
-
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -59,14 +79,16 @@ public class RobotContainer {
     OI.xButton.onTrue(Turn90Degrees);
     OI.yButton.onTrue(Turn180Degrees);
   }
-
+ 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
+
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-  return Auto;
+    return m_chooser.getSelected();
   }
+
+
 }
