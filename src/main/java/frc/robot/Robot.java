4b451974034022186252d28ee8b5fa4b3b8b25bf
superheropug <71048116+superheropug@users.noreply.github.com>
2023-02-07 18:13:40 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,7 +25,7 @@ import frc.robot.subsystems.PhotonVisionManager;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
-  //public PhotonVisionManager photon;
+ 
 
   /**
    * This function is run when the robot is first started up and should be used
