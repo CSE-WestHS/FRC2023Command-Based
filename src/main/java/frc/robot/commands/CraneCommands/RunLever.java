f191fb142f29c98f452f33b957773776a8971937
superheropug <71048116+superheropug@users.noreply.github.com>
2023-02-18 13:28:25 -0600

    Fixed implementation

--- src/main/java/frc/robot/commands/CraneCommands/RunLever.java
+++ src/main/java/frc/robot/commands/CraneCommands/RunLever.java
@@ -4,21 +4,25 @@ import frc.robot.subsystems.LeverSubsystem;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.Timer;
+import frc.robot.RobotContainer;
 
 /** An example command that uses an example subsystem. */
 public class RunLever extends CommandBase {
 
   private final LeverSubsystem lever;
   private final boolean invert;
+  private boolean hitLimit;
   /**
    * Command that runs the claw motors for a short amount of time, releasing what ever object 
    *
    * @param claw The subsystem used by this command.
    * @param invert whether motor intakes object or spits it out.
    */
-  public RunLever(LeverSubsystem lever, boolean invert) {
+  public RunLever(LeverSubsystem lever, boolean invert, boolean hitLimit) {
+    // Limit switch associated goes in last boolean, if none set it to false.
     this.lever = lever;
     this.invert = invert;
+    this.hitLimit = hitLimit;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(lever);
   }
@@ -32,8 +36,12 @@ public class RunLever extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    lever.setSpeed(Constants.LEVERSPEED);
-    if(invert){lever.setSpeed(-Constants.LEVERSPEED);}
+    if (!hitLimit){
+      lever.setSpeed(Constants.LEVERSPEED);
+      if(invert){lever.setSpeed(-Constants.LEVERSPEED);}
+  
+    }
+    
   }
 
   // Called once the command ends or is interrupted.
