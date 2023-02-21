b0163d4d8923f8f020dce6a6c5ba4150326ca667
superheropug <71048116+superheropug@users.noreply.github.com>
2023-02-21 15:05:33 -0600

    Update LimitSensors.java

--- src/main/java/frc/robot/LimitSensors.java
+++ src/main/java/frc/robot/LimitSensors.java
@@ -1,12 +1,10 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.DigitalInput;
 public class LimitSensors {
-    DigitalInput CraneSwitch = new DigitalInput(0);
+    DigitalInput CraneSwitchFront = new DigitalInput(0);
     public boolean CraneSwitched(){
         // If true, limit is switched.
-        if (CraneSwitch.get()){
-            return false;
-        }
-        return true;
+        
+        return !CraneSwitchFront.get();
     }
 }
