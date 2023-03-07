2dd9fff88c3cf20148b5818edd3d8e4a231efc22
dcsdprogrammers <dcsdprogrammers@gmail.com>
2023-03-07 16:38:38 -0600

    add CameraServer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import edu.wpi.first.cameraserver.CameraServer;
 
 
 /**
@@ -36,6 +36,7 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
     m_robotContainer.navchipManager.calibrate();
+    CameraServer.startAutomaticCapture();
 
   }
 
