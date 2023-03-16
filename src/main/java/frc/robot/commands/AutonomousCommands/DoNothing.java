5d3457541a1d453620aff6fee68483fc3c12ff0b
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2023-03-16 15:21:19 -0500

    Created a doNothing autonomous and adjusted the Auto_Drive_Speed from 0.75 to 0.5

---
+++ src/main/java/frc/robot/commands/AutonomousCommands/DoNothing.java
@@ -0,0 +1,9 @@
+package frc.robot.commands.AutonomousCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+
+public class DoNothing extends SequentialCommandGroup {
+    public DoNothing() {
+}}
